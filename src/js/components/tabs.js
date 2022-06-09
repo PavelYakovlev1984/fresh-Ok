bb33e8e4c8ef7a9ca0130ca4da65079be5167857
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2022-06-09 16:13:25 +0300

    past

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,1 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const tabs = document.querySelector('.top__filter');
+	const tabsBtn = document.querySelectorAll('.top__item');
+	const tabsContent = document.querySelectorAll('.product');
 
+	if (tabs) {
+		tabs.addEventListener('click', (e) => {
+			if (e.target.classList.contains('top__link')) {
+				const tabsPath = e.target.dataset.tabsPath;
+				tabsBtn.forEach(el => {el.classList.remove('top__link--active')});
+				document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('top__link--active');
+				tabsHandler(tabsPath);
+			}
+
+			if (e.target.classList.contains('tabs__arrow--prev')) {
+				let activeBtn = document.querySelector('.tabs__btn--active');
+				let activeParent = activeBtn.closest('.tabs__item');
+				let previousParent = activeParent.previousElementSibling;
+
+				if (previousParent) {
+					let prevActive = previousParent.querySelector('.tabs__btn')
+					tabsBtn.forEach(el => {el.classList.remove('tabs__btn--active')});
+					prevActive.classList.add('tabs__btn--active');
+
+					let path = prevActive.dataset.tabsPath;
+					tabsHandler(path);
+				}
+			}
+
+			if (e.target.classList.contains('tabs__arrow--next')) {
+				let activeBtn = document.querySelector('.tabs__btn--active');
+				let activeParent = activeBtn.closest('.tabs__item');
+				let nextParent = activeParent.nextElementSibling;
+
+				if (nextParent) {
+					let nextActive = nextParent.querySelector('.tabs__btn');
+					tabsBtn.forEach(el => {el.classList.remove('tabs__btn--active')});
+					nextActive.classList.add('tabs__btn--active');
+
+					let path = nextActive.dataset.tabsPath;
+					tabsHandler(path);
+				}
+			}
+		});
+	}
+});
